0b1a40cc42d8b9034f2f9525c17dd5dd2d197d78
Juan Ramon Montilla Sanchez <ing.juan.montilla@gmail.com>
2024-02-16 17:12:44 -0400

    Codigo inicial

---
+++ blazor01/Model/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace blazor01.Model
+{
+	public class Person
+	{
+		public string Name { get; set; }
+
+		private int Edad { get; set; }
+
+		public string LastName { get; set; }
+
+		 public void SetEdad( int NewEdad)
+		{
+
+			this.Edad = NewEdad;
+
+		}
+		public int GetEdad() =>this.Edad;
+		 
+		public string GetNombreCompleto() => this.Name + this.LastName;
+
+	}
+}
+
